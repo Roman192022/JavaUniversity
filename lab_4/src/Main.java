751eec643a0d0fe37b3bb29e97e737b27f89a43f
Роман Криворучко <121447731+Roman192022@users.noreply.github.com>
2023-12-07 22:10:33 +0200

    Add files via upload

--- lab_4/src/Main.java
+++ lab_4/src/Main.java
@@ -3,35 +3,33 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        try {
+
             // Приклад успішної валідації
-            Person person1 = new Person.PersonBuilder("John", 25, "john_doe@example.com")
+            Person person1 = new Person.PersonBuilder("John", LocalDate.of(2000, 6, 15), "john_doe@example.com")
                     .addAttendedEvent(new Concert("Rock Night", "Main Arena", LocalDate.of(2023, 8, 15), "Rock", 5, 40))
                     .addAttendedEvent(new Exhibition("Impressionist Art", "Art Museum", LocalDate.of(2023, 7, 25), "Impressionism", "Jane Doe", 80))
                     .addAttendedEvent(new TheatrePerformance("Classic Play", "City Theatre", LocalDate.of(2023, 6, 30), "Romeo and Juliet", 150, "Emily Williams"))
                     .build();
 
+            System.out.println("");
             System.out.println(person1);
+            System.out.println("");
 
-            // Приклад невдалої валідації (вік не допустимий)
-            Person person2 = new Person.PersonBuilder("Alice", 200, "alice_smith@company.org")
+            // Приклад невдалої валідації (не допустима дата народження)
+            Person person2 = new Person.PersonBuilder("Alice", LocalDate.of(2024, 1, 11), "alice_smith@company.org")
                     .build();
 
-            // Цей рядок не буде виконано через виключення
             System.out.println(person2);
+            System.out.println("");
 
 
             // Невдала валідація через недопустимий email
 
-            Person person3 = new Person.PersonBuilder("Jake", 25, "jake_email_example.com")
+            Person person3 = new Person.PersonBuilder("Jake", LocalDate.of(1999, 4, 21), "jake_email_example.com")
                     .build();
 
-            // Цей рядок не буде виконано через виключення
             System.out.println(person3);
 
 
-        } catch (IllegalArgumentException e) {
-            System.out.println("Exception: " + e.getMessage());
-        }
     }
 }
--- lab_4/src/Person.java
+++ lab_4/src/Person.java
@@ -3,36 +3,49 @@ import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.time.LocalDate;
+import java.time.Period;
 
 
 public class Person {
     private String name;
-    private int age;
+    private LocalDate dateOfBirth;
     private String email;
     private List<CulturalEvent> attendedEvents;
+    private List<String> validationErrors; // Список для зберігання повідомлень про виключення
+
 
     private Person(PersonBuilder builder) {
         this.name = builder.name;
-        this.age = builder.age;
+        this.dateOfBirth = builder.dateOfBirth;
         this.email = builder.email;
         this.attendedEvents = new ArrayList<>(builder.attendedEvents);
-
+        this.validationErrors = builder.validationErrors;
     }
 
     public static class PersonBuilder {
         private String name;
-        private int age;
+        private LocalDate dateOfBirth;
 
         private String email;
         private List<CulturalEvent> attendedEvents = new ArrayList<>();
 
-        public PersonBuilder(String name, int age, String email) {
+        private List<String> validationErrors = new ArrayList<>(); // Список для зберігання повідомлень про виключення
+
+
+        public PersonBuilder(String name, LocalDate dateOfBirth, String email) {
+
+
             validateName(name);
-            validateAge(age);
+            validateDateOfBirth(dateOfBirth);
             validateEmail(email);
+
             this.name = name;
-            this.age = age;
+            this.dateOfBirth = dateOfBirth;
             this.email = email;
+
+
+
         }
 
         public PersonBuilder addAttendedEvent(CulturalEvent event) {
@@ -42,21 +55,52 @@ public class Person {
         }
 
         public Person build() {
+            if (!validationErrors.isEmpty()) {
+                // Виведення всіх повідомлень про виключення
+                for (String error : validationErrors) {
+                    System.out.println("Validation Error: " + error);
+                }
+                System.out.println("");
+                // Повернення null, оскільки не можна створити об'єкт Person
+                return null;
+            }
 
             return new Person(this);
         }
 
         private void validateName(String name) {
             if (name == null || name.isEmpty()) {
-                throw new IllegalArgumentException("Name cannot be null or empty");
+                validationErrors.add("Name cannot be null or empty");
             }
         }
 
-        private void validateAge(int age) {
-            if (age < 0 || age > 150) {
-                throw new IllegalArgumentException("Age must be between 0 and 150");
+
+        // Валідація поля dateOfBirth
+        private void validateDateOfBirth(LocalDate dateOfBirth) {
+            LocalDate currentDate = LocalDate.now();
+            try {
+                Objects.requireNonNull(dateOfBirth);
+            } catch (Exception e) {
+                validationErrors.add("Date of birth cannot be null.");
+            }
+
+            if (dateOfBirth.isAfter(currentDate)) {
+                validationErrors.add("Date of birth cannot be in the future.");
+            }
+
+            final int MINIMUM_AGE = 6;
+            final int MAXIMUM_AGE = 130;
+
+            int age = Period.between(dateOfBirth, currentDate).getYears();
+            if (age < MINIMUM_AGE) {
+                validationErrors.add("Person is too young.");
+            }
+
+            if (age > MAXIMUM_AGE) {
+                validationErrors.add("Person is too old.");
             }
         }
+
 
         private void validateEmail(String email) {
             String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
@@ -64,13 +108,13 @@ public class Person {
             Matcher matcher = pattern.matcher(email);
 
             if (!matcher.matches()) {
-                throw new IllegalArgumentException("Invalid email format.");
+                validationErrors.add("Invalid email format.");
             }
         }
 
         private void validateEvent(CulturalEvent event) {
             if (event == null) {
-                throw new IllegalArgumentException("Attended event cannot be null");
+                validationErrors.add("Attended event cannot be null");
             }
         }
     }
@@ -82,9 +126,9 @@ public class Person {
         return name;
     }
 
-    public int getAge() {
+    public LocalDate getDateOfBirth() {
 
-        return age;
+        return dateOfBirth;
     }
 
     public String getEmail() {
@@ -103,7 +147,7 @@ public class Person {
         StringBuilder builder = new StringBuilder();
         builder.append("Person {\n");
         builder.append("   name='").append(name).append("',\n");
-        builder.append("   age=").append(age).append(",\n");
+        builder.append("   date of birth=").append(dateOfBirth).append(",\n");
         builder.append("   email='").append(email).append("',\n");
 
         builder.append("   attendedEvents=[");
@@ -127,7 +171,7 @@ public class Person {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Person person = (Person) o;
-        return age == person.age &&
+        return dateOfBirth == person.dateOfBirth &&
                 Objects.equals(name, person.name) &&
                 Objects.equals(email, person.email) &&
                 Objects.equals(attendedEvents, person.attendedEvents);
@@ -137,7 +181,7 @@ public class Person {
     @Override
     public int hashCode() {
 
-        return Objects.hash(name, age, email, attendedEvents);
+        return Objects.hash(name, dateOfBirth, email, attendedEvents);
     }
 }
 

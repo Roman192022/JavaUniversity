6ff3192d9888ea8082a541c7587e80919aeb39fb
Роман Криворучко <121447731+Roman192022@users.noreply.github.com>
2023-12-11 08:47:32 +0200

    Add files via upload

---
+++ Lab2/Lab2/src/main/java/model/CulturalEvent.java
@@ -0,0 +1,83 @@
+package model;
+
+//import java.time.LocalDate;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CulturalEvent {
+    protected String eventName;
+    protected String eventLocation;
+    //@JsonFormat(pattern = "yyyy-MM-dd")
+    //protected LocalDate eventDate;
+
+    public CulturalEvent() {
+    }
+    public CulturalEvent(String eventName, String eventLocation/*, LocalDate eventDate*/) {
+        this.eventName = eventName;
+        this.eventLocation = eventLocation;
+        //this.eventDate = eventDate;
+    }
+
+
+    // геттери
+    public String getEventName() {
+        return eventName;
+    }
+
+    public String getEventLocation() {
+        return eventLocation;
+    }
+    /*public LocalDate getEventDate() {
+        return eventDate;
+    }*/
+
+
+
+    // сеттери
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+    public void setEventLocation(String eventLocation) {
+        this.eventLocation = eventLocation;
+    }
+    /*public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }*/
+
+
+
+    public void displayEventInfo() {
+        System.out.println("Event: " + eventName);
+        System.out.println("Location: " + eventLocation);
+        //System.out.println("Date: " + eventDate);
+    }
+
+    @Override
+    public String toString() {
+        return  "   eventName='" + eventName + "',\n" +
+                "   eventLocation='" + eventLocation/* + "',\n" +
+                "   eventDate=" + eventDate*/ + "\n}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CulturalEvent that = (CulturalEvent) o;
+        return Objects.equals(eventName, that.eventName) &&
+                Objects.equals(eventLocation, that.eventLocation)/* &&
+                Objects.equals(eventDate, that.eventDate)*/;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(eventName, eventLocation/*, eventDate*/);
+    }
+}
+
---
+++ Lab2/Lab2/src/main/java/model/TheatrePerformance.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TheatrePerformance extends CulturalEvent {
+    private String playTitle;
+    private int durationInMinutes;
+    private String mainActor;
+
+    public TheatrePerformance() {
+    }
+    public TheatrePerformance(String eventName, String eventLocation, /*LocalDate eventDate,*/
+                              String playTitle, int durationInMinutes, String mainActor) {
+        super(eventName, eventLocation/*, eventDate*/);
+        this.playTitle = playTitle;
+        this.durationInMinutes = durationInMinutes;
+        this.mainActor = mainActor;
+    }
+
+
+    public void announceMainActor() {
+        System.out.println("Main actor in the theatre performance: " + mainActor);
+    }
+
+    @Override
+    public String toString() {
+        return "TheatrePerformance{\n" +
+                "   playTitle='" + playTitle + ",\n" +
+                "   durationInMinutes=" + durationInMinutes + ",\n" +
+                "   mainActor='" + mainActor + ",\n" +
+                super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TheatrePerformance that = (TheatrePerformance) o;
+        return durationInMinutes == that.durationInMinutes &&
+                Objects.equals(playTitle, that.playTitle) &&
+                Objects.equals(mainActor, that.mainActor) &&
+                super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), playTitle, durationInMinutes, mainActor);
+    }
+}
